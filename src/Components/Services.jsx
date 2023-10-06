6ac10ed943059a842ed2f43ac5c2da09c85bdd1c
Saifulkhandaker <khsaiful67@gmail.com>
2023-10-06 23:05:32 +0600

    margin added in service route

--- src/Components/Services.jsx
+++ src/Components/Services.jsx
@@ -9,8 +9,8 @@ const Services = () => {
 
     return (
         <div className="w-11/12 mx-auto">
-            <h1 className='text-3xl font-bold mt-6 text-center'>Our Event Services</h1>
-             <div className="grid md:grid-cols-2 gap-5 my-16">
+            <h1 className='text-3xl font-bold mt-6 text-left'>Our Event Services</h1>
+             <div className="grid md:grid-cols-2 gap-5 mt-5 mb-10">
             {
                 events.map(event => <Event event={event}></Event> )
             }
