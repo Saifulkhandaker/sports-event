3ed18c770fa7a842e8e680031d14a5f1eed088d0
Saifulkhandaker <khsaiful67@gmail.com>
2023-10-07 12:34:07 +0600

    banner hight updated

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -4,9 +4,9 @@ import banner  from "../assets/Images/Banner.jpg"
 const Banner = () => {
     return (
         <div>
-            <div className="hero h-[70vh]">
+            <div className="hero">
             <img
-          className="hero-overlay md:h-[70vh] inset-70 bg-white opacity-50 w-full rounded-none"
+          className="hero-overlay inset-70 bg-white opacity-50 w-full rounded-none"
           src={banner}
           alt="banner"
         />
@@ -14,8 +14,8 @@ const Banner = () => {
   <div className="hero-content text-center text-neutral-content">
     <div className="text-left">
       <p className='font-bold text-white text italic'>"We are the Event Management Specialsts"</p>
-      <h1 className="mb-5 text-5xl text-white font-bold">CELEBRATE YOUR EVENTS THAT <br /> LASTS LONGER </h1>
-      <hr className='border-t-2 border-red-300 w-[50%]' />
+      <h1 className=" text-2xl md:text-5xl text-white font-bold">CELEBRATE YOUR EVENTS THAT <br /> LASTS LONGER </h1>
+      <hr className='border-t-2 mt-4 border-red-300 w-[70%]' />
     </div>
   </div>
 </div>
