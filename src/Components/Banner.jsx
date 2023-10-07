8d8c0eecc6b63e20a7c930484dc6e3022e20f3b6
Saifulkhandaker <khsaiful67@gmail.com>
2023-10-07 12:30:07 +0600

    banner section updated

--- src/Components/Banner.jsx
+++ src/Components/Banner.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
-import banner  from "../../src/assets/Images/main_banner.jpg"
+import banner  from "../assets/Images/Banner.jpg"
 
 const Banner = () => {
     return (
         <div>
-            <div className="hero min-h-screen">
-                <img src={banner} alt="" />
-  <div className="hero-overlay bg-opacity-50"></div>
-  <div className="">
-    <div className="max-w-md">
-      <h1 className="mb-5 text-3xl md:text-7xl text-white font-bold">Extreme <br /> MountainBike <br /> Enthusiasts</h1>
+            <div className="hero h-[70vh]">
+            <img
+          className="hero-overlay md:h-[70vh] inset-70 bg-white opacity-50 w-full rounded-none"
+          src={banner}
+          alt="banner"
+        />
+  <div className="hero-overlay bg-opacity-70"></div>
+  <div className="hero-content text-center text-neutral-content">
+    <div className="text-left">
+      <p className='font-bold text-white text italic'>"We are the Event Management Specialsts"</p>
+      <h1 className="mb-5 text-5xl text-white font-bold">CELEBRATE YOUR EVENTS THAT <br /> LASTS LONGER </h1>
+      <hr className='border-t-2 border-red-300 w-[50%]' />
     </div>
   </div>
 </div>
-        </div>
+        </div>  
     );
 };
 
